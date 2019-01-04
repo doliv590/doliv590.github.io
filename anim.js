29dc53aabf1364943c00db732d9e4b8899f8e713
olivierde <olivier.delabasserue@yahoo.fr>
2019-01-04 21:09:48 +0100

    ajouté setTimeout au confirm

--- anim.js
+++ anim.js
@@ -1,9 +1,29 @@
 
-	setTimeout(function(){
-	  confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
-		{ window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}; 
+var answer
+setTimeout(function(){answer = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+if (answer)
+window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+else window.location="https://doliv590.github.io"},10000);
+
+/*var answer = confirm ("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+	if (answer) 
+		 window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+     
+    else window.location="https://doliv590.github.io"; */
+
+/*("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+	if (answer) 
+		 window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+     
+    else window.location="https://doliv590.github.io"; */
+
+/*setTimeout(function(){confirm('Hey oui ou annule')
+	if (true)
+		windon.location.replace('https://www.youtube.com');
+	else window.location='https://doliv590.github.io";}, 3000) */ 
+/* setTimeout(function(answer)confirm('Hey le site......') if (answer) window.location.replace('https://www.youtube.fr'); 
+	else window.location="https://doliv590.github.io"; 3000);  */
 
 
- ; }, 10000); 
 
 
