137025f45e5c8ebd8276095305815e3bcc3a74b1
olivierde <olivier.delabasserue@yahoo.fr>
2019-01-04 18:24:20 +0100

    settimeout ok confirm ne fonctionne pas

---
+++ anim.js
@@ -0,0 +1,9 @@
+
+	setTimeout(function(){
+	  confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+		{ window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}; 
+
+
+ ; }, 10000); 
+
+
