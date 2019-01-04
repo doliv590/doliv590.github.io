b421f1eba810fc593aa391d858d2493b0068d44c
doliv590 <46072086+doliv590@users.noreply.github.com>
2019-01-04 16:50:42 +0100

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,5 +1,5 @@
 
-if(confirm ('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please') ) {
- window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+confirm ('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please') 
+ /* window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 
  
